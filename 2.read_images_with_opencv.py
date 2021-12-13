de8ed178c571ea7280f76852ea584e4fa5c1aaa6
MECHEGUEG Iyad <79423243+Iyad2008@users.noreply.github.com>
2021-12-13 17:47:16 +0100

    Update 2.read_images_with_opencv.py

--- 2.read_images_with_opencv.py
+++ 2.read_images_with_opencv.py
@@ -2,8 +2,14 @@ import cv2
 
 img = cv2.imread("inputs/cat.jpg")
 (h, w, c) = img.shape
-print(w, h)
-res = cv2.resize(img, (w//4, h//4))
+if len(img.shape) < 3:
+    type_ = 'gray scale'
+elif len(img.shape) == 3:
+    type_ = 'Color'
+
+print(f'the image width is {w} and the image heigth is {h} and it is a {type_}')
+res = cv2.resize(img, (w // 3, h // 3))
 # print(img)
-cv2.imshow("image", res)
+cv2.imshow("img", img)
+cv2.imshow("resized", res)
 cv2.waitKey(0)
